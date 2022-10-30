5a60c15e9daf8b5c7d00e4fe63d5227005c55975
Jelisa <Jelisa@users.noreply.github.com>
2022-10-30 21:14:26 +0100

    Creación end points
    Creo los diferentes endpoints que requiere la aplicación aunque están cargados con información dummy por el momento.

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
+// Import the required modules
 const express = require('express');
 const path = require('path');
+const process = require('process');
+const {DBcompaniesBySize, DBcompaniesByCreationDate, obtainStatistics} = require('./app/app.js')
 
+// A constant to the HTML resources folder
+const HTML_RESOURCES = 'static'
+// Read the port to listen from or use a default one
+const PORT = process.argv[2] || process.env.PORT || 4000;
+// Read the hostname from the enviroment or use the localhost
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
+
+// Create the APP
 const APP = express();
 
-const PORT = 3000;
-
-APP.use(express.static('static'));
+// Indicate where the static resources can be found.
+APP.use(express.static(HTML_RESOURCES));
 
 APP.get('/', (req, res) => {
-    console.log('reuest made')
+    console.log('request made')
     res.send('working');
 });
 
-APP.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname,'static', '404.html'));
+// An end point to explain how the API works.
+APP.get('/API', (req, res) => {
+    console.log('request made')
+    res.send('working');
 });
 
-APP.listen(PORT, () => {console.log(`Listening on http://localhost:${PORT}`);});
+// Endpoint to return the companies ordered by size
+APP.get('/API/orderbySize', (req, res) => {
+    results = DBcompaniesBySize();
+    res.json(results);
+});
+
+// Endpoint to return the companies ordered by creation date
+APP.get('/API/orderbyCreationDate', (req, res) => {
+    results = DBcompaniesByCreationDate();
+    res.json(results);
+});
+
+// Endpoint to return the companies statistics
+APP.get('/API/statistics', (req, res) => {
+    results = obtainStatistics();
+    res.json(results);
+});
+
+// Other addresses rise a 404 error.
+APP.get('/*', (req, res) => {
+    console.log(path.join(__dirname,'static', '404.html'))
+    res.status(404).sendFile(path.join(__dirname,'static', '404.html'));
+});
+
+// Start to listen on the specified port.
+APP.listen(PORT, () => {console.log(`Listening on http://${HOSTNAME}:${PORT}`);});
