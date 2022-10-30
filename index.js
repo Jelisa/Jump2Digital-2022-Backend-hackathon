2f6b948b1ecf9ffa3689cb6ee5d06294c6332a8f
Jelisa <Jelisa@users.noreply.github.com>
2022-10-30 19:48:14 +0100

    basic structure
    Generate some basic queries to make sure the packages are up and running

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
+const APP = express();
 
+const PORT = 3000;
+
+APP.use(express.static('static'));
+
+APP.get('/', (req, res) => {
+    console.log('reuest made')
+    res.send('working');
+});
+
+APP.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname,'static', '404.html'));
+});
+
+APP.listen(PORT, () => {console.log(`Listening on http://localhost:${PORT}`);});
