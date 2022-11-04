2607b5adacbdaf3f77747bc7dc549ce3a2c6f86c
Jelisa <Jelisa@users.noreply.github.com>
2022-11-04 15:47:38 +0100

    Create a script to generate and populate the database
    TODO: Close the connection.

--- config/DB_population.js
+++ config/DB_population.js
@@ -6,21 +6,25 @@ import fs  from "fs";
 import PATHS from './list_paths.js';
 import { pool } from "../app/db_config.js";
 
-const initialData = loadJSONFileToObject(PATHS.initialData);
+async function populatDatabase() {
+  const initialData = loadJSONFileToObject(PATHS.initialData);
+  
+  console.log(initialData.length);
+  
+  // Populate the database with the initial Data
+  await initialData.forEach(async company => {
+      const {id, website, name, founded, size, locality, region, country, industry, linkedin_url} = company;
+      const SQL_INSERT = `INSERT INTO companies (id, website, name, founded, size, locality, region, country, industry, linkedin_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+      const [rows] = await pool.query(SQL_INSERT, [id, website, name, founded, size, locality, region, country, industry, linkedin_url]);
+      // console.log(rows);
+      if (rows.affectedRows == 0) {
+        return "Couldn't insert the data into the database";
+      }
+  });
+  // pool.end()
+}
 
-console.log(initialData.length);
-
-// Populate the database with the initial Data
-initialData.forEach(async company => {
-    const {id, website, name, founded, size, locality, region, country, industry, linkedin_url} = company;
-    const SQL_INSERT = `INSERT INTO companies (id, website, name, founded, size, locality, region, country, industry, linkedin_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-    const [rows] = await pool.query(SQL_INSERT, [id, website, name, founded, size, locality, region, country, industry, linkedin_url]);
-    console.log(rows);
-    if (rows.affectedRows == 0) {
-      return "Couldn't insert the data into the database";
-    }
-});
-// pool.end()
+populatDatabase();
 
 function loadJSONFileToObject(filepath){
   /**A function to read and parse JSON files
