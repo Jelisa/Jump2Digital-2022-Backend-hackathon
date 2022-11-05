c52970a5cc175931e8562487f84b654b26e3daa1
Jelisa <Jelisa@users.noreply.github.com>
2022-11-05 21:36:20 +0100

    Correct imports and routes
    Fix the import statement for the controller due to the renaming from api.controller to companies.controller
    
    Modify the routes to add a landing page and an API page with some information

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Import the required modules
+
 import * as url from 'url';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 import express from 'express';
@@ -7,7 +8,7 @@ import { argv, env } from 'process';
 import routes from './src/app/api_routes/api.routes.js';
 
 // A constant to the HTML resources folder
-const HTML_RESOURCES = '/src/static'
+const HTML_RESOURCES = '/src/HTML_resources'
 // Read the port to listen from or use a default one
 const PORT = argv[2] || env.PORT || 4000;
 // Read the hostname from the enviroment or use the localhost
@@ -21,11 +22,23 @@ APP.use(express.static(join(__dirname, HTML_RESOURCES)));
 
 APP.use(routes)
 
-// Other addresses rise a 404 error.
+/**  Root Webpage.*/
+APP.get('/', (req, res) => {
+    console.log('request made')
+    res.sendFile(join(__dirname, HTML_RESOURCES, 'index.html'));
+});
+
+/**  Explanatori API page*/
+APP.get('/API', (req, res) => {
+    console.log('request made')
+    res.sendFile(join(__dirname, HTML_RESOURCES, 'API.html'));
+});
+
+/**  Other addresses rise a 404 error.*/
 APP.get('/*', (req, res) => {
     console.log(join(__dirname,'static', '404.html'))
     res.status(404).sendFile(join(__dirname, HTML_RESOURCES, '404.html'));
 });
 
-// Start to listen on the specified port.
+/**  Start to listen on the specified port.*/
 APP.listen(PORT, () => {console.log(`Listening on http://${HOSTNAME}:${PORT}`);});
--- src/app/api_routes/api.routes.js
+++ src/app/api_routes/api.routes.js
@@ -1,24 +1,13 @@
 import {Router} from 'express';
-import { generateStatistics, orderbyCreationDate, orderbySize } from '../controller/api.controller.js';
+import { generateStatistics, orderbyCreationDate, orderbySize } from '../controller/companies.controller.js';
 
 const router = new Router();
 
-router.get('/', (req, res) => {
-    console.log('request made')
-    res.send('working');
-});
-
-// An end point to explain how the API works.
-router.get('/API', (req, res) => {
-    console.log('request made')
-    res.send('working');
-});
-
 // Endpoint to return the companies ordered by size
-router.get('/API/orderbySize', orderbySize);
+router.get('/API/orderBySize', orderbySize);
 
 // Endpoint to return the companies ordered by creation date
-router.get('/API/orderbyCreationDate', orderbyCreationDate);
+router.get('/API/orderByCreationDate', orderbyCreationDate);
 
 // Endpoint to return the companies statistics
 router.get('/API/statistics', generateStatistics);
