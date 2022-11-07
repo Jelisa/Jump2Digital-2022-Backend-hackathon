542c445684240344e3701c40c2ecdd7f2b99b40b
Jelisa <Jelisa@users.noreply.github.com>
2022-11-07 18:29:03 +0100

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,8 @@
+/*
+This file contains the default values for a XAMPP connection. 
+It is uploaded only as a tool to ease the set up and the trial of the software for the hackathon judges.
+It is a Security vulnerability and Will be removed once the hackathon passes.
+*/
 export const BD_ENV = {
     DB_HOST : "localhost", 
     DB_PORT : 3306, 
